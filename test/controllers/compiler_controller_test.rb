08d1776fc96bd9f731aa0e17e4a103a8f3b8e33d
Lucas Andreatta <drea.pierra@gmail.com>
2026-07-01 09:58:28 -0300

    added simple assertion tests on the main page for university class

--- test/controllers/compiler_controller_test.rb
+++ test/controllers/compiler_controller_test.rb
@@ -1,8 +1,47 @@
 require "test_helper"
 
 class CompilerControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get compiler_index_url
+  test "Deve retornar a página inicial" do
+    get root_url
     assert_response :success
+  end
+
+  test "Deve retornar o código-fonte do Hello, World! para C++" do
+    get hello_world_url(language: "cpp")
+    assert_response :success
+    assert_includes @response.body, "Hello, World!"
+  end
+
+  test "Deve compilar o código-fonte c++" do
+    post compile_url, params: {
+      source_code: "int main() { return 0; }",
+      language: "cpp"
+    }
+
+    assert_response :success
+    assert_includes @response.body, "output_frame"
+  end
+
+  test "Deve criar um snippet e retornar uma URL compartilhada" do
+    post share_url, params: {
+      source_code: "int main() { return 0; }",
+      language: "cpp",
+      compiler_flags: "-O2"
+    }
+
+    assert_response :success
+    assert_equal "application/json", @response.media_type
+    assert_match(/\/s\//, @response.body)
+    assert_equal 1, Snippet.count
+    puts @response.body
+  end
+
+  test "Deve renderizar a página de snippets" do
+    snippet = Snippet.create!(source_code: "int main() { return 0; }", language: "cpp", compiler_flags: "-O2")
+
+    get "/s/#{snippet.slug}"
+
+    assert_response :success
+    assert_includes @response.body, snippet.source_code
   end
 end
